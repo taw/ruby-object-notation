1ae97cf4958251336b28b6d464af2fff3ff8c706
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-08-28 07:07:15 +0100

    Use proper recursive implementation, not just inspect all the things

--- lib/rbon.rb
+++ lib/rbon.rb
@@ -4,7 +4,18 @@ module RBON
   Infinity = Float::INFINITY
 
   def self.encode(object)
-    object.inspect
+    case object
+    when Integer, Float, String, Symbol, NilClass, TrueClass, FalseClass
+      object.inspect
+    when Range
+      "#{encode(object.begin)}#{object.exclude_end? ? "..." : ".."}#{encode(object.end)}"
+    when Array
+      "[#{ object.map{|item| encode(item)}.join(", ") }]"
+    when Hash
+      "{#{ object.map{|key, value| "#{encode(key)} => #{encode(value)}"}.join(", ") }}"
+    else
+      raise ArgumentError, "Don't know how to serialize #{object.class}"
+    end
   end
 
   def self.decode(string)
